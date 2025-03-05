6f20f2b8791ab1f442f45707c9eff9a99cbbd950
Wolf1824 <makhijaniankush24@gmail.com>
2025-03-05 01:24:39 -0500

    writing json file for content gen req

---
+++ automate_request_input_file.py
@@ -0,0 +1,24 @@
+import string
+
+def automate_input_file_json(file_name):
+    pre_method_text = """{
+        "custom_id": "request-"""
+        
+    pre_user_content_text = """",
+        "method": "POST",
+        "url": "/v1/chat/completions",
+        "body": {"model": "gpt-4o-mini",
+        "messages": [{"role": "developer", "content": "Your responce must be a valid JSON format."},
+            {"role": "user", "content": "All the companies listed on National Stock Exchange (NSE) India, starting with the charecter """
+    post_user_content_text = """ in Json format with the following attributes, 'name, description, type'. 
+    Name - name of company, 
+    description - must include short description of company information, their main source of revenue, current holdings, what does company do? in less than 50 words.
+    type - give a label to company according to its type, like bank, technology, pharma, telecom, etc."}],
+        "max_tokens": 1000000}
+    }"""
+    for i in range(65,91):
+        charecter = chr(i)
+        final = pre_method_text + charecter + pre_user_content_text + charecter + post_user_content_text
+        with open(file_name,'a') as f:
+            f.write(final+"\n")
+            
